9833c884bf51c4b5e839f8bad92390fabd441b18
ThomasMurat <thomasmurat1992@gmail.com>
2020-05-18 18:33:55 +0200

    save

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,37 @@ var screenHeight = screen.availHeight;
 var menuHeight = document.getElementById('topMenu').clientHeight;
 var footerHeight = document.getElementById('bottomMenu').clientHeight;
 document.getElementById('contentPage').style.minHeight = screenHeight - menuHeight - footerHeight + 'px';
+//fonction montrant la présentation du produit sélectionné
+function showPresentation(price, ref, title) {
+    var textPresentation = document.getElementById(ref).innerHTML;
+    var presentationImage = document.getElementById('presentationImage');
+    var presentationTitle = document.getElementById('presentationTitle');
+    var presentationTexte = document.getElementById('presentationText');
+    var presentationPrice = document.getElementById('presentationPrice');
+    var addBasketButton = document.getElementById('addBasketButton');
+    var sectionList = document.getElementsByTagName('section');
+    for (index = 0; index < sectionList.length; index++) {
+        sectionList[index].style.display = 'none';
+    }
+    document.getElementById('presentation').style.display = 'block';
+    presentationImage.src = 'assets/img/products/' + ref;
+    presentationImage.alt = title;
+    presentationTitle.innerHTML = title;
+    presentationTexte.innerHTML = textPresentation;
+    presentationPrice.innerHTML = price + ' euros';
+    addBasketButton.setAttribute('price', price);
+    addBasketButton.setAttribute('ref', ref);
+    addBasketButton.setAttribute('title', title);
+}
+//fonction sélectionnant le thème
+
+//fonction affichant la liste des produits voulus
+function showProductList(categorie, thema){
+    var sectionList = document.getElementsByTagName('section');
+    var productList = document.getE
+    for (index = 0; index < sectionList.length; index++) {
+        sectionList[index].style.display = 'none';
+    }
+    document.getElementById('productsListe').style.display = 'block';
+
+}
